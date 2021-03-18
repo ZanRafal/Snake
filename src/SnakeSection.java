3f173e441b8efca243be220978f44dc10a8e44c3
ZanRafal <01159225@pw.edu.pl>
2021-03-18 23:57:10 +0100

    Last commit

---
+++ src/SnakeSection.java
@@ -0,0 +1,36 @@
+public class SnakeSection {
+    private int x;
+    private int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null || getClass() != obj.getClass()) return false;
+
+        SnakeSection that = (SnakeSection) obj;
+        if(x != that.x) return false;
+        if(y != that.y) return false;
+
+        return true;
+    }
+}
